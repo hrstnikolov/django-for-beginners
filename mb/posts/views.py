9b7da3fc3445064d71fadc1c3cf1744adc335aae
hrstnikolov <hrstnikolov@gmail.com>
2022-05-23 17:28:27 +0300

    added tests

--- mb/posts/views.py
+++ mb/posts/views.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from django.views.generic import ListView
 from.models import Post
 
@@ -5,4 +6,5 @@ from.models import Post
 class HomePageView(ListView):
     model = Post
     template_name: str = 'home.html'
+    context_object_name: Optional[str] = 'all_posts_list'
 
